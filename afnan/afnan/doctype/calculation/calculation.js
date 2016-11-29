fca51c8287d07c546039088bce75f78c8a3b4fe5
mhbu50 <mhbu50@gmail.com>
2016-11-29 21:55:46 +0300

    add some changing

--- afnan/afnan/doctype/calculation/calculation.js
+++ afnan/afnan/doctype/calculation/calculation.js
@@ -6,3 +6,39 @@ frappe.ui.form.on('calculation', {
 
 	}
 });
+
+		frappe.ui.form.on("Sub Frame", "frame",
+		    function(frm) {
+		        frappe.call({
+		            "method": "frappe.client.get",
+		            args: {
+		                doctype: "Item Price",
+										name:undefined,
+										filters:{"item_code":frm.doc.sub_frame[0].frame},
+		            },
+		            callback: function (data) {
+									console.log("data is = ", data.message.price_list_rate);
+									console.log("frm.doc.sub_frame[0] = ", frm.doc.sub_frame[0]);
+		                frappe.model.set_value("Sub Frame",frm.doc.sub_frame[0].name, "sub_f_price", data.message.price_list_rate);
+									 frm.refresh_field("sub_f_price");
+		            }
+		        });
+		    });
+
+				frappe.ui.form.on("Sub Mating", "mating",
+				    function(frm) {
+				        frappe.call({
+				            "method": "frappe.client.get",
+				            args: {
+				                doctype: "Item Price",
+												name:undefined,
+												filters:{"item_code":frm.doc.sub_mating[0].mating},
+				            },
+				            callback: function (data) {
+											console.log("data is = ", data.message.price_list_rate);
+											console.log("frm.doc.sub_frame[0] = ", frm.doc.sub_mating[0]);
+				                frappe.model.set_value("Sub Mating",frm.doc.sub_mating[0].name, "sub_m_price", data.message.price_list_rate);
+											 frm.refresh_field("sub_m_price");
+				            }
+				        });
+				    });
